ba7d1303b3d84afb8a7ac2bc35dd4d9581a14546
Billdex <pc.luoxin@foxmail.com>
2024-04-22 15:15:22 +0800

    markdown 消息结构定义

--- bot/openapi/message.go
+++ bot/openapi/message.go
@@ -21,15 +21,90 @@ const (
 )
 
 type PostGroupMessageReq struct {
-	Content string  `json:"content"`  // 消息文本内容
-	MsgType MsgType `json:"msg_type"` // 消息类型
-	Media   Media   `json:"media"`    // 富媒体消息，MsgType 为  MsgTypeMedia(7) 时可用
-	MsgId   string  `json:"msg_id"`   // 前置收到的用户发送过来的消息 ID，用于发送被动消息（回复）
-	MsgSeq  int     `json:"msg_seq"`  // 回复消息的序号，与 msg_id 联合使用，避免相同消息id回复重复发送，不填默认是 1。相同的 msg_id + msg_seq 重复发送会失败。
+	Content  string   `json:"content"`  // 消息文本内容
+	MsgType  MsgType  `json:"msg_type"` // 消息类型
+	Markdown Markdown `json:"markdown"` // markdown 格式消息，MsgType 为  MsgTypeMarkdown(2) 时可用
+	Keyboard Keyboard `json:"keyboard"` // 消息按钮，放在文字内容底部
+	Media    Media    `json:"media"`    // 富媒体消息，MsgType 为  MsgTypeMedia(7) 时可用
+	MsgId    string   `json:"msg_id"`   // 前置收到的用户发送过来的消息 ID，用于发送被动消息（回复）
+	MsgSeq   int      `json:"msg_seq"`  // 回复消息的序号，与 msg_id 联合使用，避免相同消息id回复重复发送，不填默认是 1。相同的 msg_id + msg_seq 重复发送会失败。
+}
+
+type Markdown struct {
+	Content string `json:"content"` // markdown 内容
+}
+
+// Keyboard 消息按钮 https://bot.q.qq.com/wiki/develop/api-v2/server-inter/message/trans/msg-btn.html
+type Keyboard struct {
+	Content KeyboardContent `json:"content"` // markdown 内容
+}
+
+type KeyboardContent struct {
+	Rows []KeyboardButtonsRow `json:"rows"`
+}
+
+type KeyboardButtonsRow struct {
+	Buttons []KeyboardButton `json:"buttons"`
+}
+
+type KeyboardButton struct {
+	Id         string                   `json:"id"`
+	RenderData KeyboardButtonRenderData `json:"render_data"` // 按钮渲染数据
+}
+
+type KeyboardButtonRenderDataStyle int
+
+const (
+	KeyboardButtonRenderDataStyleGray KeyboardButtonRenderDataStyle = 0 // 按钮样式 灰色线框
+	KeyboardButtonRenderDataStyleBlue KeyboardButtonRenderDataStyle = 1 // 按钮样式 蓝色现况
+)
+
+type KeyboardButtonRenderData struct {
+	Label        string                        `json:"label"`         // 按钮文案
+	VisitedLabel string                        `json:"visited_label"` // 点击后的按钮文案
+	Style        KeyboardButtonRenderDataStyle `json:"style"`         // 按钮样式
+	Action       KeyboardButtonAction          `json:"action"`        // 按钮响应动作
+}
+
+type KeyboardButtonActionType int
+
+const (
+	KeyboardButtonActionTypeLink        = 0 // 跳转按钮：http 或 小程序 客户端识别 scheme
+	KeyboardButtonActionTypeCallback    = 1 // 回调按钮：回调后台接口, data 传给后台
+	KeyboardButtonActionTypeInstruction = 2 // 指令按钮：自动在输入框插入 @bot data
+)
+
+type KeyboardButtonActionAnchor int
+
+const (
+	KeyboardButtonActionAnchorQImage = 1 // 唤起手 Q 选图器
+)
+
+type KeyboardButtonAction struct {
+	Type          KeyboardButtonActionType       `json:"type"`           // 按钮类型
+	Permission    KeyboardButtonActionPermission `json:"permission"`     // 按钮权限
+	Data          string                         `json:"data"`           // 操作相关的数据
+	Reply         bool                           `json:"reply"`          // 指令按钮可用 指令是否带引用回复本消息
+	Enter         bool                           `json:"enter"`          // 指令按钮可用 点击按钮后直接自动发送 data
+	Anchor        KeyboardButtonActionAnchor     `json:"anchor"`         // 指令按钮可用 设置后后会忽略 action.enter 配置
+	UnSupportTips string                         `json:"unsupport_tips"` // 客户端不支持本 action 时候弹出的 toast文案
+}
+
+type KeyboardButtonActionPermissionType int
+
+const (
+	KeyboardButtonActionPermissionTypeSpecialUser = 0 // 指定用户可操作
+	KeyboardButtonActionPermissionTypeAdmin       = 1 // 仅管理者可操作
+	KeyboardButtonActionPermissionTypeAllUser     = 2 // 所有人可操作
+)
+
+type KeyboardButtonActionPermission struct {
+	Type           KeyboardButtonActionPermissionType `json:"type"`             // 权限类型
+	SpecifyUserIds []string                           `json:"specify_user_ids"` // 有权限的用户 id 列表
 }
 
 type Media struct {
-	FileInfo string `json:"file_info"`
+	FileInfo string `json:"file_info,omitempty"`
 }
 
 func (p *PostGroupMessageReq) Method() string {
